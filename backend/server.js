366b61affbb6ef7ce754124c15af511ec636c192
VanshhShah <codebyvansh@gmail.com>
2026-06-21 22:07:39 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const supabase = require("./supabase");
+
 
 const spotify = require("./spotify");
 const getChessData = require("./chess");      
